952f76800ef535d9734e4007dad988439d477cc8
HasanSbaih <H.sbaih93@gmail.com>
2017-03-03 18:06:45 +0200

    Configurations

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname+'/app/index.html'));
-app.listen(process.env.PORT ||3000);
+app.listen(process.env.PORT ||3010);
