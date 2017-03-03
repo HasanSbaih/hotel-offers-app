8a405ba4ab63e804adb3586231a7667883a5be64
HasanSbaih <H.sbaih93@gmail.com>
2017-03-03 18:48:13 +0200

    Configurations

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
 var app = express();
-app.use(express.static(__dirname+'/app/index.html'));
-app.listen(process.env.PORT ||3010);
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
