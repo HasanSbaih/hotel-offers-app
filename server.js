f4716e209886645a6fe5a278edf71b72a7e430c7
HasanSbaih <H.sbaih93@gmail.com>
2017-03-03 17:02:06 +0200

    cloud configurations

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname+'/src/app'));
+app.use(express.static(__dirname+'/app'));
 app.listen(process.env.PORT ||3000);
