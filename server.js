6cfc01a19f1b9a6e3ea76eb485e4b5a1063a1ebe
HasanSbaih <H.sbaih93@gmail.com>
2017-03-03 21:46:18 +0200

    Configurations for deployment on heroku

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
-var express = require("express");
-var app = express();
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
