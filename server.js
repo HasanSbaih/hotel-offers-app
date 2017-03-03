641bf695318cba36f23f13ad854f06f9a8c309ee
HasanSbaih <H.sbaih93@gmail.com>
2017-03-03 18:52:22 +0200

    Confg

--- server.js
+++ server.js
@@ -1,13 +1,2 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
-
-var CONTACTS_COLLECTION = "contacts";
-
-var app = express();
-app.use(bodyParser.json());
-
-// Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
